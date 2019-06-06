d7c885083be7ffbe6387b7ffa89189ae002a5870
duoduolee <lishaolint@gmail.com>
2019-06-06 11:54:05 +0800

    add the postgres for the flask web

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+from models import Result
 
 
 @app.route('/')
 def hello():
-    return "this is the datananlysis website by flask"
+    return "Hello World!"
 
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-    print(os.environ['APP_SETTINGS'])
-
-
-
 
 
 
